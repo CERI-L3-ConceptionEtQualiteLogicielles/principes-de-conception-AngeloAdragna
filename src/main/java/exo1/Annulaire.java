ce25716029b6f5f76675025cbb38afc9d07b61ff
AngeloAdragna <angelo.adragna@alumni.univ-avignon.fr>
2025-02-13 08:23:38 +0100

    Correction S du principe solid

---
+++ src/main/java/exo1/Annulaire.java
@@ -0,0 +1,35 @@
+package exo1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Annulaire implements ContactService {
+    private static List<Contact> lesContacts = new ArrayList<>();
+
+    @Override
+    public void ajouteContact(Contact contact) {
+        lesContacts.add(contact);
+    }
+
+    @Override
+    public void supprimeContact(Contact contact) {
+        lesContacts.remove(contact);
+    }
+
+    @Override
+    public void envoiEmail(Contact contact, String message) {
+        // Logique pour envoyer un email
+    }
+
+    @Override
+    public void afficheContacts() {
+        for (Contact contact : lesContacts) {
+            System.out.println("Nom: " + contact.getNom() + ", Numéro: " + contact.getNumero());
+        }
+    }
+
+    public void sauvegardeEnBD() {
+        // Logique pour sauvegarder les contacts dans une base de données
+    }
+
+}
