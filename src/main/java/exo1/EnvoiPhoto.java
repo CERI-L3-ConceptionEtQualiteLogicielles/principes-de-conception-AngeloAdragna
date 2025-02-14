3721cc53ba9481f0c8830cf5945e959206490611
AngeloAdragna <angelo.adragna@alumni.univ-avignon.fr>
2025-02-14 11:27:58 +0100

    diagramme de classe

--- src/main/java/exo1/EnvoiPhoto.java
+++ src/main/java/exo1/EnvoiPhoto.java
@@ -3,7 +3,7 @@ package exo1;
 import java.awt.*;
 
 public class EnvoiPhoto implements SendService{ //extends ServiceEnvoiPhoto
-    private final ServiceEnvoiPhoto service = new ServiceEnvoiPhoto();
+    private final ServiceEnvoiPhoto service = new ServiceEnvoiPhoto(); //méthode 2
     public void envoiMessage(Contact contact, String image) {
         //envoiPhoto(contact,image); //méthode 1
         service.envoiPhoto(contact,image); //méthode 2
